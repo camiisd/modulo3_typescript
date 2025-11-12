fb772af76cda315a6853fe60d9b6941496eb9ada
camiisd <liccamilasoto@gmail.com>
2025-11-12 17:58:21 -0300

    Se comparte el ejercicio 2 de la clase 2

---
+++ clase2/ejercicio2.ts
@@ -0,0 +1,14 @@
+/**Ejercicio 2: Uso de funciones con parámetros opcionales
+Consigna: Define una función llamada saludar que acepte un parámetro nombre de tipo string y otro opcional saludo de tipo string. 
+Si el parámetro saludo no es proporcionado, la función debe usar "Hola" por defecto. Retorna el saludo y muestra el resultado en la consola. */
+
+function saludar (nombre: string, saludo?: string) : string {
+    if (!saludo) {
+        return `Hola ${nombre}`
+    } else {
+        return `${saludo} ${nombre}!`;
+    }
+}
+
+console.log(saludar('Camila', 'Holi'))
+console.log(saludar('Gonzalo'))
