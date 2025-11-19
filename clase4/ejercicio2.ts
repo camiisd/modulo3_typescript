61d8887516198401cee3370b6aa11bb11d4af564
camiisd <liccamilasoto@gmail.com>
2025-11-18 22:33:03 -0300

    Se comparte el ejercicio 2 de la clase 4

---
+++ clase4/ejercicio2.ts
@@ -0,0 +1,8 @@
+/**Actividad 2: Usar For...Of para recorrer los valores de un arreglo
+Crea un arreglo con al menos 5 números y usa un bucle for...of para imprimir cada número en consola. */
+
+const arreglo: number[] = [1, 2, 3, 4, 5]
+
+for (let numero of arreglo) {
+    console.log(`${numero}`)
+}
